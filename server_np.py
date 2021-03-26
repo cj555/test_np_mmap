4e19eed2084c2cb70afed726dc73e8fb80067cbe
cj555 <chengjuan555@gmail.com>
2021-03-26 10:21:19 +0800

    Update server_np.py

--- server_np.py
+++ server_np.py
@@ -2,10 +2,12 @@ import numpy as np
 import time 
 filename = 'test_np.dat'
 fp = np.memmap(filename, dtype='float32', mode='w+', shape=(3,4))
+fp = np.memmap(filename, dtype='<U100', mode='w+', shape=(3,4)) # 传输字符串,U:字符串，100 字符串的最大长度
 print(fp)
 
 for i in range(100):
-    data = np.ones(12)*i
+    # data = np.ones(12)*i # float
+    data= np.array([f'{i}-{i}']*12) # 字符串
     data.resize((3,4))
     fp[:] = data[:]
     print('server:',fp)
